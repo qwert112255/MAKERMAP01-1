d33f37fcb9bd0566b1ca3c0766b29756e484ab8a
emma119018 <emma119018@gmail.com>
2018-05-20 01:39:47 +0800

    底部導航

--- app/src/main/java/com/example/emma119018/makermap/MapsActivity.java
+++ app/src/main/java/com/example/emma119018/makermap/MapsActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -22,6 +23,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private BottomBar mBottomBar;
+
+       @Override
+       protected void onCreate(Bundle savedInstanceState) {
+              super.onCreate(savedInstanceState);
+               setContentView(R.layout.activity_maps);
+               // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+                      SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+               mapFragment.getMapAsync(this);
+           }
 
     /**
      * Manipulates the map once available.
@@ -741,8 +751,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         CustomInfoWindowAdapter adapter = new CustomInfoWindowAdapter(MapsActivity.this);
         googleMap.setInfoWindowAdapter(adapter);
-
-
     }
 
 }
